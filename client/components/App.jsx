728037a93a3112b18354240e26f989b15c541b24
Alex Burkowsky <aburkowsky9@gmail.com>
2019-01-30 14:19:00 -0800

    add eslint and react boilerplate

---
+++ client/components/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Chart from 'chart.js';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+    };
+  }
+
+  renderChart() {
+  }
+
+  render() {
+    return ();
+  }
+}
+
+export default App;
