a78456baad72148d341015c255b9f3240ed25d96
Alex Burkowsky <aburkowsky9@gmail.com>
2019-01-30 16:20:21 -0800

    read data from csv and create data array

--- server/index.js
+++ server/index.js
@@ -1,5 +1,9 @@
 const express = require('express');
 const path = require('path');
+const fs = require('fs');
+const csv = require('csv-parser');
+
+const NBAData = [];
 
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -9,3 +13,12 @@ app.listen(PORT, () => {
 });
 
 app.use('/', express.static(path.join(__dirname, '../public')));
+
+fs.createReadStream('sample_data/nba.csv')
+  .pipe(csv())
+  .on('data', (data) => {
+    NBAData.push(data);
+  })
+  .on('end', () => {
+    console.log(NBAData.filter((row) => row['Visitor/Neutral'] === 'Portland Trail Blazers').length);
+  });
